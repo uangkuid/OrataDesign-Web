bfc144ea43d11029861caedd8dc730d06433d31a
oratakashi <oratakashi@oratakashi.com>
2025-12-13 11:37:46 +0700

    Initial Commit

---
+++ composeApp/src/webMain/kotlin/com/oratakashi/design/docs/main.kt
@@ -0,0 +1,11 @@
+package com.oratakashi.design.docs
+
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.window.ComposeViewport
+
+@OptIn(ExperimentalComposeUiApi::class)
+fun main() {
+    ComposeViewport {
+        App()
+    }
+}
