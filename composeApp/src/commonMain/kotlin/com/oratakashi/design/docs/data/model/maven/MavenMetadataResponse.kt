a08b746b8aa7a3e34ef45ee8f15488cdd722ac99
Oratakashi Nhamako <davidcreazy@gmail.com>
2026-01-01 20:00:20 +0700

    [UK-128] fixbug: xml parser

--- composeApp/src/commonMain/kotlin/com/oratakashi/design/docs/data/model/maven/MavenMetadataResponse.kt
+++ composeApp/src/commonMain/kotlin/com/oratakashi/design/docs/data/model/maven/MavenMetadataResponse.kt
@@ -1,33 +1,43 @@
 package com.oratakashi.design.docs.data.model.maven
 
 import kotlinx.serialization.Serializable
+import nl.adaptivity.xmlutil.serialization.XmlElement
 import nl.adaptivity.xmlutil.serialization.XmlSerialName
-import nl.adaptivity.xmlutil.serialization.XmlChildrenName
 
-/**
- * Response model untuk Maven metadata XML
- */
 @Serializable
 @XmlSerialName("metadata", "", "")
 data class MavenMetadataResponse(
+    @XmlElement
     val groupId: String = "",
+    @XmlElement
     val artifactId: String = "",
+    @XmlElement
+    @XmlSerialName("versioning", "", "")
     val versioning: Versioning = Versioning()
 )
 
 @Serializable
+@XmlElement(true)
 @XmlSerialName("versioning", "", "")
 data class Versioning(
+    @XmlElement
+    @XmlSerialName("latest")
     val latest: String = "",
+    @XmlElement
+    @XmlSerialName("release")
     val release: String = "",
+    @XmlElement(true)
+    @XmlSerialName("versions")
     val versions: Versions = Versions(),
+    @XmlElement
+    @XmlSerialName("lastUpdated")
     val lastUpdated: String = ""
 )
 
 @Serializable
 @XmlSerialName("versions", "", "")
 data class Versions(
-    @XmlChildrenName("version", "", "")
+    @XmlElement(true)
+    @XmlSerialName("version", "", "")
     val version: List<String> = emptyList()
 )
-
