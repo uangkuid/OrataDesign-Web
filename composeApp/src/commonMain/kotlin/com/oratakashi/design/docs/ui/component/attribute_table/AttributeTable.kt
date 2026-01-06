69c689b53a50496565fbd3c06522f1be94f70c8f
oratakashi <oratakashi@oratakashi.com>
2026-01-06 19:21:11 +0700

    [UK-128] feature: add Attribute Table Component

--- composeApp/src/commonMain/kotlin/com/oratakashi/design/docs/ui/component/attribute_table/AttributeTable.kt
+++ composeApp/src/commonMain/kotlin/com/oratakashi/design/docs/ui/component/attribute_table/AttributeTable.kt
@@ -1,47 +1,122 @@
 package com.oratakashi.design.docs.ui.component.attribute_table
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import com.oratakashi.design.docs.ui.component.code.InlineCode
 import com.seanproctor.datatable.DataColumn
 import com.seanproctor.datatable.material3.DataTable
+import dev.snipme.highlights.model.SyntaxLanguage
 
 @Composable
 fun AttributeTable(
-    modifier: Modifier = Modifier
+    data: List<AttributeData> = emptyList(),
+    modifier: Modifier = Modifier,
 ) {
-    BoxWithConstraints {
+    BoxWithConstraints(
+        modifier = modifier
+    ) {
         val isWideScreen = maxWidth > 1200.dp
 
         val column = mutableListOf(
             DataColumn {
-                Text("Name")
+                Text(
+                    text = "Name",
+                    modifier = Modifier
+                        .padding(vertical = 8.dp)
+                )
             },
             DataColumn {
-                Text("Description")
+                Text(
+                    text = "Description",
+                    modifier = Modifier
+                        .padding(vertical = 8.dp)
+                )
             },
             DataColumn {
-                Text("Default")
+                Text(
+                    text = "Default",
+                    modifier = Modifier
+                        .padding(vertical = 8.dp)
+                )
             },
             DataColumn {
-                Text("Required")
+                Text(
+                    text = "Required",
+                    modifier = Modifier
+                        .padding(vertical = 8.dp)
+                )
             },
         )
 
         if (isWideScreen) {
             column += DataColumn {
-                Text("Control")
+                Text(
+                    text = "Control",
+                    modifier = Modifier
+                        .padding(vertical = 8.dp)
+                )
             }
         }
 
         DataTable(
             columns = column,
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.fillMaxWidth(),
+            rowHeight = Dp.Unspecified,
         ) {
+            data.forEach {
+                row {
+                    cell {
+                        InlineCode(
+                            code = it.name,
+                            language = SyntaxLanguage.KOTLIN,
+                            modifier = Modifier
+                                .padding(vertical = 8.dp)
+                        )
+                    }
+                    cell {
+                        Text(
+                            text = it.description,
+                            modifier = Modifier
+                                .padding(vertical = 8.dp)
+                        )
+                    }
 
+                    cell {
+                        Text(
+                            text = it.defaultValue ?: "",
+                            modifier = Modifier
+                                .padding(vertical = 8.dp)
+                        )
+                    }
+                    cell {
+                        Text(
+                            text = it.required.toString(),
+                            modifier = Modifier
+                                .padding(vertical = 8.dp)
+                        )
+                    }
+
+                    if (isWideScreen) {
+                        cell {
+                            Box(
+                                modifier = Modifier
+                                    .padding(vertical = 8.dp)
+                                    .width(200.dp)
+                            ) {
+                                it.control()
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
--- composeApp/src/commonMain/kotlin/com/oratakashi/design/docs/ui/screen/content/alert/AlertScreen.kt
+++ composeApp/src/commonMain/kotlin/com/oratakashi/design/docs/ui/screen/content/alert/AlertScreen.kt
@@ -10,10 +10,16 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.oratakashi.design.component.alert.OraInfoAlert
+import com.oratakashi.design.component.textfield.OraTextField
 import com.oratakashi.design.docs.navigation.page.AlertNavigation
+import com.oratakashi.design.docs.ui.component.attribute_table.AttributeData
 import com.oratakashi.design.docs.ui.component.attribute_table.AttributeTable
 import com.oratakashi.design.docs.ui.component.component_preview.ComponentPreview
 import com.oratakashi.design.docs.ui.component.content_section.ContentSection
@@ -28,6 +34,106 @@ fun AlertScreen(
     showBack: Boolean = false
 ) {
     val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
+    val data: List<AttributeData> = listOf(
+        AttributeData(
+            name = "name",
+            description = "The title text of the alert",
+            required = true,
+            defaultValue = "-",
+            control = {
+                var inputState by remember { mutableStateOf("") }
+                OraTextField(
+                    value = inputState,
+                    onValueChange = { inputState = it },
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+        ),
+        AttributeData(
+            name = "description",
+            description = "The description text of the alert",
+            required = false,
+            defaultValue = "-",
+            control = {
+                var inputState by remember { mutableStateOf("") }
+                OraTextField(
+                    value = inputState,
+                    onValueChange = { inputState = it },
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+        ),
+        AttributeData(
+            name = "visible",
+            description = "Controls the visibility of the alert with fade animation",
+            required = false,
+            defaultValue = "-",
+            control = {
+                var inputState by remember { mutableStateOf("") }
+                OraTextField(
+                    value = inputState,
+                    onValueChange = { inputState = it },
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+        ),
+        AttributeData(
+            name = "showCloseIcon",
+            description = "Whether to show the close icon",
+            required = false,
+            defaultValue = "-",
+            control = {
+                var inputState by remember { mutableStateOf("") }
+                OraTextField(
+                    value = inputState,
+                    onValueChange = { inputState = it },
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+        ),
+        AttributeData(
+            name = "onClose",
+            description = "Callback Whether to show the close icon",
+            required = false,
+            defaultValue = "-",
+            control = {
+                var inputState by remember { mutableStateOf("") }
+                OraTextField(
+                    value = inputState,
+                    onValueChange = { inputState = it },
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+        ),
+        AttributeData(
+            name = "action",
+            description = "The optional action composable to be displayed",
+            required = false,
+            defaultValue = "-",
+            control = {
+                var inputState by remember { mutableStateOf("") }
+                OraTextField(
+                    value = inputState,
+                    onValueChange = { inputState = it },
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+        ),
+        AttributeData(
+            name = "icon",
+            description = "The optional icon to be displayed, defaults to info icon",
+            required = false,
+            defaultValue = "-",
+            control = {
+                var inputState by remember { mutableStateOf("") }
+                OraTextField(
+                    value = inputState,
+                    onValueChange = { inputState = it },
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+        )
+    )
     DetailContent(
         scrollBehavior = scrollBehavior,
         onBackClick = onBackClick,
@@ -65,7 +171,9 @@ fun AlertScreen(
                 ContentSection(
                     title = "Attributes",
                     content = {
-                        AttributeTable()
+                        AttributeTable(
+                            data = data
+                        )
                     }
                 )
             }
