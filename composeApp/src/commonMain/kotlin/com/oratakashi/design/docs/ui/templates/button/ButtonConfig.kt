10e34fd4000fe0405476f120279027bfb55879a2
Oratakashi Nhamako <davidcreazy@gmail.com>
2026-01-30 22:26:42 +0700

    [UK-128] draft: spinner

---
+++ composeApp/src/commonMain/kotlin/com/oratakashi/design/docs/ui/templates/button/ButtonConfig.kt
@@ -0,0 +1,8 @@
+package com.oratakashi.design.docs.ui.templates.button
+
+data class ButtonConfig(
+    val label: String,
+    val isEnabled: Boolean,
+    val showIconLeft: Boolean,
+    val showRightIcon: Boolean
+)
